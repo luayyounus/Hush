d5bc1b969d194faf17f03471227fba4f1dc59988
Luay Younus <luayghazwan@yahoo.com>
2017-06-26 23:35:45 -0700

    Chat Room Model created with id and name

---
+++ Hush/ChatRoom.swift
@@ -0,0 +1,19 @@
+//
+//  ChatRoom.swift
+//  Hush
+//
+//  Created by Luay Younus on 6/26/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+import Foundation
+
+class ChatRoom {
+    let id: String
+    let name: String
+    
+    init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
