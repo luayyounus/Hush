df83727a014be7beb9eb26c44194bb5d5f1377ed
Luay Younus <luayghazwan@yahoo.com>
2017-06-27 20:55:41 -0700

    ChatRoom NibCell created with ChatRoom

---
+++ Hush/ChatRoomTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChatRoomTableViewCell.swift
+//  Hush
+//
+//  Created by Luay Younus on 6/27/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomTableViewCell: UITableViewCell {
+
+    var chatRoom : ChatRoom! {
+        didSet {
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
