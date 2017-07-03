354dec44a79e31eb13633ee4e7e0907a962f0ab8
Luay Younus <luayghazwan@yahoo.com>
2017-07-02 21:47:00 -0700

    Adding New chat room without reloading Firebase

--- Hush/NewChatRoomViewController.swift
+++ Hush/NewChatRoomViewController.swift
@@ -13,23 +13,37 @@ class NewChatRoomViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("the ref: \(String(describing: chatRoomRef))")
     }
     
-private lazy var chatRoomRef: DatabaseReference = Database.database().reference().child("chatRooms")
-    @IBOutlet weak var hushNameField: UITextField!
+    var chatRoomRef: DatabaseReference?
+    
+    var chatRoomToAppend: ChatRoom?
+    
+    @IBOutlet weak var chatRoomName: UITextField!
+    
     @IBAction func doneButtonPressed(_ sender: UIButton) {
     
-        if let name = hushNameField?.text {
-            let newChannelRef = chatRoomRef.childByAutoId()
+        if let name = chatRoomName?.text {
+            let chatRoomAutoId = chatRoomRef?.childByAutoId()
             let singleChatRoom = [
                 "name": name
             ]
-            newChannelRef.setValue(singleChatRoom)
+            chatRoomAutoId?.setValue(singleChatRoom)
+            chatRoomToAppend = ChatRoom(id: (chatRoomAutoId?.key)!, name: name)
+            self.dismiss(animated: true, completion: nil)
         }
-        
-        self.dismiss(animated: true, completion: nil)
-        ChatRoomsViewController.shared.newChatRoomAddedFlag = true
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+    
+        if segue.identifier == ChatRoomsViewController.identifier {
+            guard let destination = segue.destination as? ChatRoomsViewController else { return }
+            destination.chatRoom.append(chatRoomToAppend!)
+        }
+    }
+    
     @IBAction func cancelButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
