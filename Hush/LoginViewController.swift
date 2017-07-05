709103ff7e993735c270c90378997f0da5faa68e
Luay Younus <luayghazwan@yahoo.com>
2017-07-04 20:57:45 -0700

    User Defaults for Name LoggedIn

--- Hush/LoginViewController.swift
+++ Hush/LoginViewController.swift
@@ -26,6 +26,8 @@ class LoginViewController: UIViewController {
                 }
             })
         }
+        let userName = UserDefaults.standard
+        userName.set(self.nameTextField.text, forKey: "userName")
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
