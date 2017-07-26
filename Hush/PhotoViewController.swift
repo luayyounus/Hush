85fd12b87d97e5ba5ecc50986087ceca31c59155
Luay Younus <luayghazwan@yahoo.com>
2017-07-26 03:54:27 -0700

    Hide NavController and Status bar when Image Tapped

--- Hush/PhotoViewController.swift
+++ Hush/PhotoViewController.swift
@@ -18,11 +18,23 @@ class PhotoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.hidesBarsOnTap = true
+
         self.imageView.image = imageToView?.image
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        if self.navigationController?.isNavigationBarHidden == true {
+            return true
+        }
+        return false
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return UIStatusBarAnimation.slide
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
-        
     }
 }
